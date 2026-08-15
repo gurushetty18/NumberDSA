5b5190c87424147c870465b30848720d7e0bada1
Gurukumar <gurushetty018@gmail.com>
2026-08-15 19:16:03 +0530

    added selection sorting

--- src/sorting_techniques/BubbleSort.java
+++ src/sorting_techniques/BubbleSort.java
@@ -1,4 +1,4 @@
-package sorting_algorithms;
+package sorting_techniques;
 
 
 
---
+++ src/sorting_techniques/SelectionSort.java
@@ -0,0 +1,25 @@
+package sorting_techniques;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+public static void main(String[] args) {
+	int[] arr = {5, 3, 8, 4, 2};
+	
+	for (int i = 0; i < arr.length; i++) {
+		int minIndex = i ;
+		
+		for (int j =i + 1; j < arr.length; j++) {
+			if ( arr[j]  < arr[minIndex]) {
+				minIndex = j ;
+			}
+		}
+		
+		int temp = arr[i];
+		arr[i] = arr[minIndex];
+		arr[minIndex] = temp;
+	}
+	
+	System.out.println(Arrays.toString(arr));
+}
+}
