e51bc7acdb6c2174cc2c4aa5677f6be3b2df49df
Gurukumar <gurushetty018@gmail.com>
2026-08-25 23:34:01 +0530

    implimented logic

--- src/welocmeToStrings/RotateArray.java
+++ src/welocmeToStrings/RotateArray.java
@@ -7,6 +7,7 @@ public class RotateArray {
 	public static void rotate(int[] nums, int k) {
 
 		int n = nums.length;
+		k = k % n ;
 		reverse(nums, 0, n - 1);
 		reverse(nums, 0, k - 1);
 		reverse(nums, k, n - 1);
